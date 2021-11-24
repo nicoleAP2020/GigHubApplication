e5b7a3493b6119c46514338b39fbe9eeac75bade
User <User@DESKTOP-DQVN2MB>
2021-11-24 23:52:58 +0200

    Greate a gig form

---
+++ GigHubApplication/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GigHubApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHubApplication.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
